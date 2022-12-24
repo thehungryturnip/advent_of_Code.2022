d28ab6392d4ef91f1cd8cfbbd02e6b8c9130a4b0
The Hungry Turnip <thehungryturnip@gmail.com>
2022-12-23 22:59:58 -0500

    chore: setup prettier, eslint, lint-staged, and husky

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": "off",
+  },
+  plugins: ["prettier"],
+};
