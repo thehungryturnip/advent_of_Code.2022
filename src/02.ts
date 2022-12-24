9ca7feaa32f76efcfb720d113c192669758a7e3d
The Hungry Turnip <thehungryturnip@gmail.com>
2022-12-24 02:47:23 -0500

    feat: solved day 2 part 2

--- src/02.ts
+++ src/02.ts
@@ -23,3 +23,26 @@ const rounds = data.split("\n");
 const scores = rounds.map((round) => scoreTable[round]);
 const score = utils.sumArray(scores);
 console.log(`Day 02 Part 1: Total score is ${score}.`);
+
+// A: Rock
+// B: Paper
+// C: Scissor
+// X: lose
+// Y: draw
+// Z: win
+const scoreTable2: Record<string, number> = {
+  "A X": 3 + 0,
+  "B X": 1 + 0,
+  "C X": 2 + 0,
+  "A Y": 1 + 3,
+  "B Y": 2 + 3,
+  "C Y": 3 + 3,
+  "A Z": 2 + 6,
+  "B Z": 3 + 6,
+  "C Z": 1 + 6,
+  "": 0,
+};
+
+const scores2 = rounds.map((round) => scoreTable2[round]);
+const score2 = utils.sumArray(scores2);
+console.log(`Day 02 Part 2: Total score is ${score2}.`);
