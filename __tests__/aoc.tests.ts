37b82605375a0149635891941e339979055c080b
The Hungry Turnip <thehungryturnip@gmail.com>
2022-12-24 23:57:02 -0500

    feat: solve day 3 part 2

--- __tests__/aoc.tests.ts
+++ __tests__/aoc.tests.ts
@@ -8,6 +8,7 @@ it.each([
   [2, 1, 8890, day02.score],
   [2, 2, 10238, day02.score2],
   [3, 1, 7701, day03.dupSum],
+  [3, 2, 2644, day03.badgeSum],
 ])(
   "For day %p part %p. Expect %p.",
   (day: number, part: number, answer: number, actual: number) => {
--- src/03.ts
+++ src/03.ts
@@ -5,30 +5,42 @@ const stringToPriority = (s: string): number[] => {
   const priorities: number[] = [];
   for (const c of s) {
     const priority = c.charCodeAt(0) - priorityBase;
-    priorities.push(priority > 0 ? priority : priority + 58);
+    priorities.push(priority >= 0 ? priority : priority + 58);
   }
   return priorities;
+};
+
+const findCommon = (this_: number[], that: number[]): number[] => {
+  const common = new Set<number>();
+  const set = new Set<number>(this_);
+  for (const n of that) {
+    if (set.has(n)) {
+      common.add(n);
+    }
+  }
+  return Array.from(common);
 };
 
 const data = utils.readFile(3);
 
 const bagStr = data.split("\n").filter((s) => s !== "");
-const bagCompStr = bagStr.map((s) => {
+const bagPris = bagStr.map((s) => stringToPriority(s));
+const bagCompPris = bagPris.map((s) => {
   const half = Math.floor(s.length / 2);
   return [s.slice(0, half), s.slice(half)];
 });
-const bagCompPris = bagCompStr.map((strings) => [
-  stringToPriority(strings[0]),
-  stringToPriority(strings[1]),
-]);
-const dups = bagCompPris.map((pris) => {
-  const set = new Set<number>(pris[0]);
-  for (const n of pris[1]) {
-    if (set.has(n)) {
-      return n;
-    }
-  }
-  return 0;
-});
+const dups = bagCompPris.map((pris) => findCommon(pris[0], pris[1])[0]);
+
 export const dupSum = dups.reduce((sum, n) => sum + n, 0);
 console.log(`Day 03 Part 1: The priority sum is ${dupSum}.`);
+
+const groups = [];
+for (let i = 0; i < bagPris.length; i += 3) {
+  groups.push(bagPris.slice(i, i + 3));
+}
+
+const badges = groups.map(
+  (group) => findCommon(findCommon(group[0], group[1]), group[2])[0]
+);
+export const badgeSum = badges.reduce((sum, n) => sum + n, 0);
+console.log(`Day 03 Part 2: The badge sum is ${badgeSum}.`);
